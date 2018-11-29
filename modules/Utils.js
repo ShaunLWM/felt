37480c65bd8fc58cb75f2570d05f634660f515bc
ShaunLWM <shaunlimweimin@gmail.com>
2018-11-29 11:44:19 +0800

    Remove seconds from datetime

--- modules/Utils.js
+++ modules/Utils.js
@@ -4,7 +4,7 @@ module.exports = {
     processPost: function (p) {
         return {
             ...p,
-            date: moment(p.date).format('MMMM Do YYYY, h:mm:ss a'),
+            date: moment(p.date).format('MMMM Do YYYY, h:mm a'),
             tags: p.tags.length < 1 ? 'none' : (p.tags.reduce((accu, curr, i, arr) => {
                 accu += `<a href="/t/${curr}">${curr}</a>`;
                 if (i < (arr.length - 1)) {
