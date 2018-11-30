2546787463163eff622c3592c6f3a2902e5f4aa4
ShaunLWM <shaunlimweimin@gmail.com>
2018-11-30 13:57:01 +0800

    Fix XSS vulnerability

--- modules/Utils.js
+++ modules/Utils.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const escape = require('escape-html');
 
 module.exports = {
     processPost: function (p) {
@@ -6,7 +7,7 @@ module.exports = {
             ...p,
             date: moment(p.date).format('MMMM Do YYYY, h:mm a'),
             tags: p.tags.length < 1 ? 'none' : (p.tags.reduce((accu, curr, i, arr) => {
-                accu += `<a href="/t/${curr}">${curr}</a>`;
+                accu += `<a href="/t/${curr}">${escape(curr)}</a>`;
                 if (i < (arr.length - 1)) {
                     accu += ', '
                 }
