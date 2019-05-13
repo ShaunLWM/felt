d8f584b76400446af648fe481d4102bf13a3111c
ShaunLWM <shaunlimweimin@gmail.com>
2019-05-14 00:25:23 +0800

    Refactoring

--- public/js/global.js
+++ public/js/global.js
@@ -175,59 +175,29 @@ $(document).ready(function () {
     });
 
     $("#editor").froalaEditor({
-        // Set the image upload parameter.
+        placeholderText: "",
         imageUploadParam: "image",
-
-        // Set the image upload URL.
         imageUploadURL: "/admin/upload",
-
-        // Additional upload params.
         imageUploadParams: { id: "my_editor" },
-
-        // Set request type.
         imageUploadMethod: "POST",
-
-        // Set max image size to 5MB.
         imageMaxSize: 5 * 1024 * 1024,
-
-        // Allow to upload PNG and JPG.
         imageAllowedTypes: ["gif", "jpeg", "jpg", "png", "svg", "blob"]
-    })
-        .on("froalaEditor.image.beforeUpload", function (e, editor, images) {
-            // Return false if you want to stop the image upload.
+    }).on("froalaEditor.image.beforeUpload", function (e, editor, images) {
         })
         .on("froalaEditor.image.uploaded", function (e, editor, response) {
-            // Image was uploaded to the server.
         })
         .on("froalaEditor.image.inserted", function (e, editor, $img, response) {
-            // Image was inserted in the editor.
         })
         .on("froalaEditor.image.replaced", function (e, editor, $img, response) {
-            // Image was replaced in the editor.
         })
         .on("froalaEditor.image.error", function (e, editor, error, response) {
-            // Bad link.
             if (error.code == 1) { }
-
-            // No link in upload response.
             else if (error.code == 2) { }
-
-            // Error during image upload.
             else if (error.code == 3) { }
-
-            // Parsing response failed.
             else if (error.code == 4) { }
-
-            // Image too text-large.
             else if (error.code == 5) { }
-
-            // Invalid image type.
             else if (error.code == 6) { }
-
-            // Image can be uploaded only to same domain in IE 8 and IE 9.
             else if (error.code == 7) { }
-
-            // Response contains the original server response to the request if available.
         });
 
     $("#button-post-submit, #button-post-save-draft").on("click", function (e) {
@@ -256,10 +226,10 @@ $(document).ready(function () {
         }
 
         $.post(route, opts, function (data) {
-            M.toast({ html: "Successfully posted. Refreshing in 5s..", duration: 10000 });
+            M.toast({ html: "Successfully posted. Refreshing in 3s..", duration: 10000 });
             setTimeout(() => {
                 location.reload();
-            }, 5000);
+            }, 3000);
         }).fail(function (data) {
             $("input").attr("disabled", false);
             $("#editor").froalaEditor("edit.on");
